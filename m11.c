4cb622b52b9ee809ad873895911b27afaec5fa95
manojeminem <35653185+manojeminem@users.noreply.github.com>
2018-01-26 16:39:05 +0530

    Create m11.c

---
+++ m11.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    char a;
+    scanf("%c",&a);
+        if(a!='1'&&a!='2'&&a!='3'&&a!='4'&&a!='5'&&a!='6'&&a!='7'&&a!='8'&&a!='9'&&a!='0')
+        {
+        printf("Alphabet");
+        }
+        else
+        {
+        printf("No");
+        }
+        
+    
+}
