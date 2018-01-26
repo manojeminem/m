32bc227d6e0800bcb55255b06e7f7f75243733bd
manojeminem <35653185+manojeminem@users.noreply.github.com>
2018-01-26 16:55:00 +0530

    Create m5.c

---
+++ m5.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("Enter the number A");
+scanf("%d",&a);
+printf("Enter the number B");
+scanf("%d",&b);
+printf("Enter the number c");
+scanf("%d",&c);
+printf("The largest number is:%d",&a);
+}
